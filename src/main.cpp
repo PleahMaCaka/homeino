00ab9c75aefd3241dbcb23348989b67f0a3f9d79
pleahmacaka <pleahmacaka@gmail.com>
2023-04-29 10:19:50 +0900

    dht code

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,11 @@
 
 DHT dht = DHT(DHT_PIN, DHT11);
 
+unsigned char humid = 0; // humidity
+float temp = 0;          // temperature
+
+unsigned int UPSTACK = 0;
+
 void setup()
 {
     Serial.begin(9600);
@@ -17,5 +22,14 @@ void setup()
 
 void loop()
 {
-    
+    UPSTACK += 1;
+
+    humid = dht.readHumidity();
+    temp = dht.readTemperature();
+
+    Serial.print("Humidity: " + String(humid) + "%\t");
+    Serial.print("Temperature: " + String(temp, 1) + "°C");
+    Serial.println("\t[ " + String(UPSTACK) + " ]");
+
+    delay(10000);
 }
